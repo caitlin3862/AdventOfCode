65c51a018080d1e3b16b731b10ebe56e2a43f1f1
caitlin3862 <145706806+caitlin3862@users.noreply.github.com>
2024-12-02 14:02:22 -0500

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day2 {
+    public static void main(String[] args) {
+
+        int safe = 0;
+        ArrayList<String> fileData = getFileData("src/Day1Input.txt");
+        for (int i = 0; i < fileData.size(); i++){
+            String[] report = fileData.get(i).split(" ");
+            for (int j = 0; j < report.length; j++){
+                // check the conditions
+            }
+        }
+
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
+}
